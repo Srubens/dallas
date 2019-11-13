a45d230e66b6ed19eac179093c46edd7fb6b9d8b
srubens <rubensfilipe6@gmail.com>
2019-11-13 15:17:41 -0300

    alterando o scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 $(function(){
     
-    $('[data-js="contact-form"]').on("submit", function(e){
+    $('[data-js="contact-form"]').submit(function(e){
         e.preventDefault();
         
         let contactForm = $(this);
         
         $.post(contactForm.attr("action"), contactForm.serialize()).then(function(){
             alert(`Obrigado por entrar em contato ${contactForm.name} retornaremos em breve!`);
-            let redirect = "https://www.agenciadallas.com.br";
-            $(location).attr('href', redirect);
+            $(location).attr('href', "https://www.agenciadallas.com.br");
         });
 
     });

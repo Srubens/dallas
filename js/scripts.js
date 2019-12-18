c1d742c146608fab74181ea4016000fd9413b259
Rubens Filipe <rubensfilipe6@gmail.com>
2019-12-18 14:14:24 -0300

    removendo o form

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,6 @@
 $(function(){
 
     "use script";
-
-    // $('[data-js="contact-form"]').validate({
-    //     rules:{
-    //         name:{
-    //             maxlength:20,
-    //             minlength:3,
-    //             required:true
-    //         },
-    //         email:{
-    //             required:true,
-    //             emial:true
-    //         },
-    //         titulo:{
-    //             required: true
-    //         },
-    //         message:{
-    //             required:true
-    //         }
-    //     },
-    //     messages:{
-    //         name:"Opa! Favor colocar um nome valido!",
-    //         email:"Opa! Favor colocar um email valido!",
-    //         titulo:"Coloque ao menos do que se trata o assunto!",
-    //         message:"Ao menos diga um Oi",
-    //     },
-    //     submitHandler: function (form){
-    //         form.submt()
-    //     }
-    // });
     
     $('[data-js="contact-form"]').submit(function(e){
         e.preventDefault();
